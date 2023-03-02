8838ca82f8c5716f1c3482fc4bec3fcd37a30b13
Dan Holodak <danholodak@gmail.com>
2023-03-02 16:47:42 -0500

    write bind

--- intro_problems.js
+++ intro_problems.js
@@ -25,4 +25,45 @@ function addNumbers(sum, numsLeft, completionCallback) {
     }
 }
 
-addNumbers(0, 3, sum => console.log(`Total Sum ${sum}`));
+// addNumbers(0, 3, sum => console.log(`Total Sum ${sum}`));
+
+Function.prototype.myBind = function(context, ...bindArgs){
+    let that = this
+    return function(...callArgs){
+            that.call(context, ...bindArgs, ...callArgs)
+    }
+}
+class Lamp {
+    constructor() {
+      this.name = "a lamp";
+    }
+  }
+  
+  const turnOn = function() {
+    console.log("Turning on " + this.name);
+  };
+  
+  const lamp = new Lamp();
+  
+//   turnOn(); // should not work the way we want it to
+  
+  const boundTurnOn = turnOn.bind(lamp);
+  const myBoundTurnOn = turnOn.myBind(lamp);
+  
+//   boundTurnOn(); // should say "Turning on a lamp"
+//   myBoundTurnOn(); // should say "Turning on a lamp"
+
+
+function askIfGreaterThan(el1, el2, callback){
+    rl.question(`Is ${el1} greater than ${el2}? (yes or no)`, function(answer){
+        if (answer === "yes"){
+            callback(true);
+        }else {
+            callback(false);
+        }
+        
+    });
+}
+function innerBubbleSortLoop(array, i, madeAnySwaps, outerBubbleSortLoop){
+    
+}
